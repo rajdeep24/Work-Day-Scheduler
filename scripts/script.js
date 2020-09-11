f651fc3c07f2448901dd91dc94e4de0dfae21d7c
Developer Raj <rajdeepkakar@gmail.com>
2020-09-11 08:49:06 -0400

    Created a current hour varibale that is assigned to the moment().formant('ha'). Created an object array called timeBlocks. Total of 9 objects and each object contains two properties and values. The first property represents the time and second represents the event.

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,52 @@ $(document).ready(function () {
 	// THEN the current day is displayed at the top of the calendar
 	var currentDay = moment().format("dddd, MMMM Do");
 	$("#currentDay").text(currentDay);
+
+	// WHEN I scroll down
+	// THEN I am presented with timeblocks for standard business hours
+	currentHour = moment().format("ha"); //This is the format to display hour+am/pm
+
+	//Create array of time blocks and each time block has two properties on it representing the timeblock label and the event.
+	var timeBlocks = [
+		{
+			time: "9am",
+			event: "test",
+		},
+		{
+			time: "10am",
+			event: "test",
+		},
+		{
+			time: "11am",
+			event: "test",
+		},
+		{
+			time: "12pm",
+			event: "test",
+		},
+		{
+			time: "1pm",
+			event: "test",
+		},
+		{
+			time: "2pm",
+			event: "test",
+		},
+		{
+			time: "3pm",
+			event: "test",
+		},
+		{
+			time: "4pm",
+			event: "test",
+		},
+		{
+			time: "5pm",
+			event: "test",
+		},
+	];
+
+	console.log(timeBlocks);
 	//-----------------------DOM Variables-------------------//
 
 	//--------------------Function Definitions----------------//
@@ -21,14 +67,6 @@ $(document).ready(function () {
 
 // ## Acceptance Criteria
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-var currentDay = moment().format("dddd, MMMM Do");
-console.log(currentDay);
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
