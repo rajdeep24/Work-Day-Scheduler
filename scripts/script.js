9b6ad4bac9ca7acda6734dc0defcb0f65df26955
Developer Raj <rajdeepkakar@gmail.com>
2020-09-11 07:39:56 -0400

    Tested various date formats from the moment.js library. Added the current day to the javascript to display below the work day scheduler title.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,39 @@
-//-----------------------Variables-------------------//
+$(document).ready(function () {
+	//-----------------------Variables-------------------//
+	// GIVEN I am using a daily planner to create a schedule
+	// WHEN I open the planner
+	// THEN the current day is displayed at the top of the calendar
+	var currentDay = moment().format("dddd, MMMM Do");
+	$("#currentDay").text(currentDay);
+	//-----------------------DOM Variables-------------------//
 
-//-----------------------DOM Variables-------------------//
+	//--------------------Function Definitions----------------//
 
-//--------------------Function Definitions----------------//
+	//-----------------------Function Calls---------------------//
 
-//-----------------------Function Calls---------------------//
+	//----------------------Event Listeners---------------------//
+});
+// ## User Story
 
-//----------------------Event Listeners---------------------//
+// AS AN employee with a busy schedule
+// I WANT to add important events to a daily planner
+// SO THAT I can manage my time effectively
+
+// ## Acceptance Criteria
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+var currentDay = moment().format("dddd, MMMM Do");
+console.log(currentDay);
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
